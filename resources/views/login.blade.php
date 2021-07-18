cbfe8bb161d91ad56c0695370e6941d84f1f3d0e
Dustin Tran <dustin.andy.tran@gmail.com>
2021-07-18 21:34:58 +0900

    FEAT: create view skeleton with endpoints: home, login, signup, product

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>LOGIN</title>
+
+        
+    </head>
+    <body class="login-body">
+        LOGIN BODY
+        NAVIGATE: HOME
+        EXECUTE: LOGIN Fx
+    </body>
+</html>
---
+++ resources/views/product.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>PRODUCT</title>
+
+        
+    </head>
+    <body class="login-body">
+        PRODUCT BODY
+        NAVIGATE: HOME
+        EXECUTE: ACCESS PRODUCT CRUD
+    </body>
+</html>
