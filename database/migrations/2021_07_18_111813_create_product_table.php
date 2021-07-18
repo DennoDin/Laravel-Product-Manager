d5cb2d8c509509fe950ebc50d5e4577ece61bbd5
Dustin Tran <dustin.andy.tran@gmail.com>
2021-07-18 20:49:51 +0900

    FEAT: Migration: create product table

---
+++ database/migrations/2021_07_18_111813_create_product_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->string('SKU');
+            $table->string('name');
+            $table->string('product_detail')->charset('ascii');
+            $table->binary('image'); // Blob
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
