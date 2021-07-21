ca83f902906bf143b348c859a644c73c556e4d72
Dustin Tran <dustin.andy.tran@gmail.com>
2021-07-21 20:47:17 +0900

    FEAT: added Breeze starter kit

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { 
-    return view('home');
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('login', function() {
-    return view('login');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::get('signup', function() {
-    return view('signup');
-});
-
-Route::get('product', function() {
-    return view('product');
-});
+require __DIR__.'/auth.php';
