49b27d0de68bae89e859ed617a89c70098e592d8
Dustin Tran <dustin.andy.tran@gmail.com>
2021-07-22 23:11:21 +0900

    FEAT: Add product management page to navbar and routes

---
+++ resources/views/products.blade.php
@@ -0,0 +1,11 @@
+<x-app-layout>
+    <x-slot name="header">
+        {{ __('Products') }}
+    </x-slot>
+    <div class="navbar">
+        <a href="/">Home</a>
+    </div>
+    <h1>Product Management</h1>
+    PRODUCT BODY
+    EXECUTE: ACCESS PRODUCT CRUD
+</x-app-layout>
