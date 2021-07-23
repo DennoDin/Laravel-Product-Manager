b5af637e29379d0a4eeecf7e8a562b88ee0a5d26
Dustin Tran <dustin.andy.tran@gmail.com>
2021-07-23 20:08:33 +0900

    WIP: started product Create request, controller, and blade

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -2,10 +2,29 @@
     <x-slot name="header">
         {{ __('Products') }}
     </x-slot>
-    <div class="navbar">
-        <a href="/">Home</a>
-    </div>
-    <h1>Product Management</h1>
-    PRODUCT BODY
-    EXECUTE: ACCESS PRODUCT CRUD
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="p-6 bg-white border-b border-gray-200">
+                <!-- PRODUCT BODY
+                EXECUTE: ACCESS PRODUCT CRUD -->
+
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                <form method="POST" action="{{ route('addProduct') }}">
+                    @csrf
+
+                    <!-- Name -->
+                    <div>
+                        <x-label for="name" :value="name" />
+                        <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                    </div>
+                    <!-- SKU -->
+                    
+                    <!-- Description -->
+                    
+                    <!-- Image Upload -->
+                </form>
+            </div>
+        </div>
+    </div>  
 </x-app-layout>
