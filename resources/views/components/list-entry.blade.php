f2bf400f60f4a1d62ae12c70f77de2785f28fcf1
Dustin Tran <dustin.andy.tran@gmail.com>
2021-07-28 14:17:26 +0900

    FEAT: add word break to prevent overflow

--- resources/views/components/list-entry.blade.php
+++ resources/views/components/list-entry.blade.php
@@ -3,10 +3,10 @@
         <!-- TODO: image here -->
         <!-- <img src="{{ $image }}" -->
     <!-- </span> -->
-    <span class="p-3 col-start-2 col-end-3">{{ $name ?? ''}}</span>
-    <span class="p-3 col-start-3 col-end-4">{{ $sku ?? ''}}</span>
-    <span class="p-3 col-start-4 col-end-8">{{ $description ?? '' }}</span>
-    <span class="p-3 col-start-8 col-end-9">
+    <span class="p-3 col-start-2 col-end-3 break-words">{{ $name ?? ''}}</span>
+    <span class="p-3 col-start-3 col-end-4 break-words">{{ $sku ?? ''}}</span>
+    <span class="p-3 col-start-4 col-end-8 break-words">{{ $description ?? '' }}</span>
+    <span class="p-3 col-start-8 col-end-9 break-words">
         <span class="p-1 bg-white border-b ml-3">
             <a href="products/{{$sku}}/edit">Edit</a>
         </span>
